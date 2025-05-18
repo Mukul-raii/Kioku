01e8b081287d3d1477fc672d9e74fddf8755a85c
Mukul Rai <mukurai2004@gmail.com>
2025-05-18 23:31:16 +0530

    feat: update package.json and tsup.config.ts for module type and logger configuration; enhance turbo.json pipeline structure

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -4,5 +4,7 @@ export default defineConfig((options: Options) => ({
   entryPoints: ["src/index.ts"],
   clean: true,
   format: ["cjs"],
+  external: [], // <- override default, include all packages
+  noExternal: ['@repo/logger'],
   ...options,
 }));
